e3742d01c9fecc0f0ad878f6493e4254dd94d475
usa-glitch <usa@eatrading.group>
2026-05-25 19:12:44 +0200

    Add RootLayout component for application layout

---
+++ src/app/layout.tsx
@@ -0,0 +1,10 @@
+import { Inter } from "next/font/google";
+import "./globals.css";
+const inter = Inter({ subsets: ["latin"] });
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="en" className="scroll-smooth">
+      <body className={inter.className} style={{ margin: 0 }}>{children}</body>
+    </html>
+  );
+}
